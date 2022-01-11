e3666e472784e0aad6b4b08c8ab949cb96756d97
bharaths18 <57673167+bharaths18@users.noreply.github.com>
2022-01-11 13:43:07 +0530

    Added GET and UPDATE user(s)

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -3,6 +3,15 @@ const crypto = require('crypto');
 const User = require('../Models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
     console.log('Inside signup');
@@ -103,3 +112,36 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // 4) Log user in using JWT
   // createSendToken(user, 200, res);
 });
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updatePassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
+//Get user(s)
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
+// Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,6 +6,15 @@ const router = express.Router();
 router.post('/signup', userController.signup);
 router.post('/forgotPassword', userController.forgotPassword);
 router.patch('/resetPassword/:token', userController.resetPassword);
+//Add Protect Middleware here 
 router.patch('/updateMyPassword', userController.updatePassword);
-
+router.patch('/updateMe', userController.updateMe);
+//ADMIN AUTH REQUIRED HERE
+router
+  .route('/')
+  .get(userController.getAllUsers);
+router
+  .route('/:uid')
+  .get(userController.getUser)
+  .patch(userController.updateUser);
 module.exports = router;
